fb4d0fa0c2498db4184ee3dc03a35a3e8932dbac
maggo <me@mar.co.de>
2025-03-24 17:14:21 +0100

    add accountAssociation

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -2,6 +2,13 @@ export async function GET() {
   const appUrl = process.env.NEXT_PUBLIC_URL;
 
   const config = {
+    accountAssociation: {
+      header:
+        "eyJmaWQiOjEwMTc4LCJ0eXBlIjoiY3VzdG9keSIsImtleSI6IjB4NEM0YmUwODUxNGJGQTU1QjgwRjNjMzA1ZWM2MDg4NDk2MDQ0NjQ3MiJ9",
+      payload: "eyJkb21haW4iOiJhd2Vzb21lLWZyYW1lLXN0YXJ0ZXIudmVyY2VsLmFwcCJ9",
+      signature:
+        "MHgyOGM2ZTAzMzMxZjJkOTVkNGIyNWU4YmZkMTIzOGQ0NGM1ODEyOTJhNTgyNTQ4NjhiYTYzZDhmZDJmNzI2NjEyMDRhNzEzOTQ4NzJkYzZjMGZkN2E0NGMyNzE4YjZiNmY3YWZmNGJlYjE0NzEyN2FmMzIxY2MxYjcxNWJkYWE5MzFj",
+    },
     frame: {
       version: "1",
       name: "Frames v2 Demo",
